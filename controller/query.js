75bfa7bceeb65a0371f52ee108b5c9d4cf2fe7b2
Zeitgeisth <mrsubasht@gmail.com>
2019-07-03 22:53:34 +0545

    a bit updated

--- controller/query.js
+++ controller/query.js
@@ -8,11 +8,17 @@ pool.getConnection((err, connection) => {
     if(err) throw err;
     console.log('Database Connected');
 
+    //Create Database
+    // connection.query("CREATE DATABASE exam_package_management_system", (err, result)=>{
+    //     if(err) throw err;
+    //     console.log("Database Created");
+    // });
+
     const queryString = "CREATE TABLE assignment (assignmentID INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), contact VARCHAR(10), address VARCHAR(255), dateOfAssignment VARCHAR(20), dateOfSubmission VARCHAR(20), noOfPackets INT, packageCode VARCHAR(5))";
     connection.query(queryString, (err, result)=>{
         if(err) throw err;
         console.log("Table created");
-    })
+    });
     connection.release();
 });
 
