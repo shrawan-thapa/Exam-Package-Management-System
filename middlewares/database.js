5bb14730af48a7637af02ab49c6125638b6a94af
SK537 <sabalkc462@gmail.com>
2019-07-19 21:12:30 +0545

    Modified tables

--- middlewares/database.js
+++ middlewares/database.js
@@ -1,12 +1,11 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
-const pool  = mysql.createPool({
-    connectionLimit : 10,
-    host            : 'localhost',
-    user            : 'root',
-    password        : 'root',
-     database        : 'exam_package_management_system'
-  });
-  
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "exam_package_management_system"
+});
 
 module.exports.pool = pool;
